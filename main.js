5fa15db393b56aa08ed1858573cd41909f62fdfc
Naseef Mashood <nosfeeferatu@gmail.com>
2024-06-27 12:14:30 +0530

    about content and restructuring styles

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 import './styles/modern-normalize.css'
 import './styles/style.css'
+import './styles/components/header.css'
+import './styles/components/hero.css'
+import './styles/components/about.css'
