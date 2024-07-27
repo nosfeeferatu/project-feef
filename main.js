a87e7d25229cb1d7e3129ffc6206515792a89079
Naseef Mashood <nosfeeferatu@gmail.com>
2024-07-27 18:56:23 +0530

    moved css from js to head

--- main.js
+++
@@ -1,6 +0,0 @@
-import './styles/modern-normalize.css'
-import './styles/style.css'
-import './styles/components/header.css'
-import './styles/components/hero.css'
-import './styles/components/about.css'
-import './styles/components/projects.css'
